88cb5610b0819e9e4ae06fe1689c7ed84ec99a93
ultracoconut <ivanosky_617@hotmail.com>
2025-09-11 18:16:26 +0200

    update init_paymen_history.js

--- public/init_pages/init_payment_history.js
+++ public/init_pages/init_payment_history.js
@@ -9,6 +9,9 @@ export function initPaymentHistory(){
    //DOM elements
    const prevBtn = document.getElementById('prev-page-btn');
    const nextBtn = document.getElementById('next-page-btn');
+   const closeBtn = document.getElementById('close-detail-box');
+   const overlay = document.getElementById('overlay');
+   const paymentDetailBox = document.getElementById('payment-detail-box');
    
    //Setup buttons
    prevBtn.addEventListener('click', async () => {
@@ -25,7 +28,6 @@ export function initPaymentHistory(){
        }
    });
 
-
   nextBtn.addEventListener('click', async () => {
     try {
       
@@ -39,6 +41,12 @@ export function initPaymentHistory(){
        }
   });
 
-    //Call paymentHistory Controller at start
-     paymentHistoryController();
+  //Close button
+  closeBtn.addEventListener('click', () => {
+    paymentDetailBox.style.display = 'none';
+    overlay.style.display = 'none';
+  });
+
+  //Call paymentHistory Controller at start
+  paymentHistoryController();
 }
