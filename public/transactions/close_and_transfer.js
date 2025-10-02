af3f74a738105879f1e2deddd2ff34a4b7c2640d
ultracoconut <ivanosky_617@hotmail.com>
2025-10-02 21:04:29 +0200

    clear code close_and_transfer.js

--- public/transactions/close_and_transfer.js
+++ public/transactions/close_and_transfer.js
@@ -65,7 +65,7 @@ export async function closeAndTransfer (sourceAddress, recipientAddress) {
 
     for (const currency of nonZeroBalances) {
       if (currency === 'DOT') {
-        tx = apiAH.tx.balances.transferAllowDeath(recipientAddress, balances['DOT']);
+        tx = apiAH.tx.balances.transferAllowDeath(recipientAddress, balances[currency]);
      
       } else if (ASSETS_ID[currency]) { //Native asset
           tx = apiAH.tx.assets.transfer(ASSETS_ID[currency], recipientAddress, balances[currency]);
@@ -123,7 +123,7 @@ export async function closeAndTransfer (sourceAddress, recipientAddress) {
 
     for (const currency of nonZeroBalances) {
       if (currency === 'DOT') {
-        tx = apiAH.tx.balances.transferAllowDeath(recipientAddress, balances['DOT'].sub(feeBatch.muln(2)));
+        tx = apiAH.tx.balances.transferAllowDeath(recipientAddress, balances[currency].sub(feeBatch.muln(2)));
 
       } else if (ASSETS_ID[currency]) { //Native asset
           tx = apiAH.tx.assets.transfer(ASSETS_ID[currency], recipientAddress, balances[currency]);
