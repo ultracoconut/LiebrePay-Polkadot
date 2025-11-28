06e0e55e31a3521b7bba3104b69c96584cd5b966
ultracoconut <ivanosky_617@hotmail.com>
2025-11-28 17:48:03 +0100

    Add support to DAI-snowbridge currency in constants.js

--- public/constants.js
+++ public/constants.js
@@ -1,7 +1,8 @@
  export const SUPPORTED_CURRENCIES = [
   'DOT', 
   'USDT', 
-  'USDC'
+  'USDC',
+  'DAI'
  ];
  
  //Native assets in Asset Hub (with numeric AssetId)
@@ -12,6 +13,26 @@
 
  //Foreign assets in Asset Hub (with MultiLocation XCM)
  export const MULTILOCATION = {
+  DAI: {
+  parents: 2,
+  interior: {
+    X2: [
+      {
+        globalConsensus: {
+          ethereum: {
+            chainId: 1
+          }
+        }
+      },
+      {
+        accountKey20: {
+          network: null,
+          key: "0x6b175474e89094c44da98b954eedeac495271d0f"
+        }
+      }
+    ]
+  }
+}
 
  };
 
@@ -19,7 +40,8 @@
  export const DECIMAL = {
    DOT: 10,
    USDT: 6,
-   USDC: 6
+   USDC: 6,
+   DAI:18
  };
  
 //Header keys in the multi-payment .csv file
@@ -56,21 +78,24 @@ export function initializeConstants() {
  const DEC_BN = {
  DOT: new BN(10).pow(new BN(10)), // 10^10 (Polkadot)
  USDT: new BN(10).pow(new BN(6)), // 10^6 (Stablecoins)
- USDC: new BN(10).pow(new BN(6)) // 10^6 (Stablecoins)
+ USDC: new BN(10).pow(new BN(6)), // 10^6 (Stablecoins)
+ DAI: new BN(10).pow(new BN(18)) // 10^18 (DAI)
  };
 
  //Minimum free balance required in LiebrePay (bn)
  MIN_BAL_FREE = { 
     DOT: DEC_BN.DOT.muln(2).divn(100), //0.02 DOT
     USDT: DEC_BN.USDT.muln(2).divn(100), //0.02 USDT
-    USDC: DEC_BN.USDC.muln(2).divn(100) //0.02 USDC
+    USDC: DEC_BN.USDC.muln(2).divn(100), //0.02 USDC
+    DAI: DEC_BN.DAI.muln(2).divn(100) //0.02 DAI
   }; 
 
  //Minimum amount to pay in LiebrePay (bn)
  MIN_PAY_AMOUNT = {
    DOT: DEC_BN.DOT.divn(100), //0.01 DOT
    USDT: DEC_BN.USDT.divn(100), //0.01 USDT
-   USDC: DEC_BN.USDC.divn(100) //0.01 USDC
+   USDC: DEC_BN.USDC.divn(100), //0.01 USDC
+   DAI: DEC_BN.DAI.divn(100) //0.01 DAI
  };
 
 
